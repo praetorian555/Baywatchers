257dcf624e52b92d0e6ccdc7f8d2b8d6ef080e4d
Jovan Blanuša <jovan.blanusa@gmail.com>
2016-04-11 23:46:11 +0200

    Primer rada sa RS485 komunikacijom

--- eurobot_lib/main_serial.c
+++ eurobot_lib/main_serial.c
@@ -1,21 +1,11 @@
 /**
   ******************************************************************************
-  * @file    Examples/GPIOToggle/main.c 
-  * @author  MCD Application Team
+  * @file    main_serial.c 
+  * @author  Jovan Blanuša
   * @version V1.0.0
-  * @date    09/13/2010
-  * @brief   Main program body.
+  * @date    11/04/2015
+  * @brief   Glavni program za serijsku komunikaciju.
   ******************************************************************************
-  * @copy
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2010 STMicroelectronics</center></h2>
   */ 
 
 /* Includes ------------------------------------------------------------------*/
